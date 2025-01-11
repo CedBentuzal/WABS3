e7c6ff68bb6b096eb18cf71549cefc6cf694b272
CedBentuzal <bentuzalcedlouise@gmail.com>
2025-01-12 03:38:27 +0800

    Updated connect_to_db to use DATABASE_URL for Heroku deployment

--- dbs_connect.py
+++ dbs_connect.py
@@ -1,15 +1,19 @@
+import os
 import asyncpg
 
 async def connect_to_db():
     try:
-        conn = await asyncpg.connect(
-            user='postgres',
-            password='mitsutech',
-            database='postgres',
-            host='localhost',
-            port=5432
-        )
+        
+        DATABASE_URL = os.getenv("DATABASE_URL")
+
+        
+        if not DATABASE_URL:
+            raise ValueError("DATABASE_URL is not set. Set it in your environment variables.")
+
+        
+        conn = await asyncpg.connect(DATABASE_URL)
         return conn
     except Exception as e:
         print(f"Error connecting to the database: {e}")
         return None
+
